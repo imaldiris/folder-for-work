3d72e71bb374221c85e5229071fe0bc6227a42b0
Andrew <a.latbipov@gmail.com>
2020-10-02 23:18:49 +0300

    ex14.py

---
+++ ex14.py
@@ -0,0 +1,21 @@
+def stop_time(glob_time, moment):
+    """время остановки на светофоре"""
+    if (glob_time//moment) % 2 == 0:
+        stop_time = moment * (glob_time//moment + 1) - glob_time
+        return stop_time
+    else:
+        return 0
+
+
+def Unmanned(l, n, track):
+    current_distance = 0
+    global_time = 0
+    for i in track:
+        current_distance = i[0] - current_distance
+        global_time += current_distance
+        global_time += stop_time(global_time, i[1])
+        current_distance = i[0]
+    global_time += l - track[-1][0]
+    return global_time
+
+
